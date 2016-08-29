2d19dcbf5908f17cea7ad1da0c7964789db2d952
GSDimitrov <german.s.d@gmail.com>
2016-08-29 14:07:09 +0300

    fix

--- app/Http/Requests/UploadBook.php
+++ app/Http/Requests/UploadBook.php
@@ -30,7 +30,7 @@ class UploadBook extends Request
              'isbn10' => 'required|min:10|max:10|unique:books', // is it column or table
              'isbn13' => 'required|min:13|max:13|unique:books',
              'genre'  => 'required|max:255',
-             'cover' => 'required',
+             // 'cover' => 'required',
              'cover' => 'required|mimes:jpeg,jpg,png,gif|dimensions:width=250,height=400',
          ];
 
